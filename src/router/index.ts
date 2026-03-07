c1a2b59fe779280d635c5cbce75f1cb82908e370
plus-hubb <xasdangkhchnaphav@gmail.com>
2026-03-08 00:01:44 +0700

    fix bug api database

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter,createWebHistory} from "vue-router"
+import {createRouter,createWebHashHistory} from "vue-router"
 
 import Dashboard from "../pages/admin/dashboard.vue"
 import Doctors from "../pages/admin/doctors.vue"
@@ -42,7 +42,7 @@ component:ManageGroups
 ]
 
 const router = createRouter({
-history:createWebHistory(),
+history:createWebHashHistory(),
 routes
 })
 
