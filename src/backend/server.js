31114dbe83a869ca34a75282a89f5853b732ee12
plus-hubb <xasdangkhchnaphav@gmail.com>
2026-03-09 02:30:10 +0700

    fix express wildcard route

--- src/backend/server.js
+++ src/backend/server.js
@@ -14,7 +14,9 @@ const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname, "../../dist")))
 
-app.get("*", (req,res)=>{
+app.use(express.static(path.join(__dirname,"../../dist")))
+
+app.use((req,res)=>{
 res.sendFile(path.join(__dirname,"../../dist/index.html"))
 })
 
