31eea3d8f4cab131970ac0681895a12533064ca2
plus-hubb <xasdangkhchnaphav@gmail.com>
2026-03-09 03:04:34 +0700

    fix host binding

--- src/backend/server.js
+++ src/backend/server.js
@@ -195,8 +195,8 @@ app.get("/", (req,res)=>{
   res.send("Hospital API running")
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", ()=>{
-  console.log("Server running on port " + PORT)
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
