b4be0c1224f7baa2fde28ef139d90e5ac39f4895
plus-hubb <xasdangkhchnaphav@gmail.com>
2026-03-09 02:25:35 +0700

    serve frontend

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,22 @@
 import express from "express"
 import cors from "cors"
 import mysql from "mysql2"
+import path from "path"
+import { fileURLToPath } from "url"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, "../../dist")))
+
+app.get("*", (req,res)=>{
+res.sendFile(path.join(__dirname,"../../dist/index.html"))
+})
 
 const db = mysql.createConnection({
   host: "centerbeam.proxy.rlwy.net",
